7d161be5692adedd9737f11f399e40e680e3d1ad
juan10-7 <juansantana1@colegioguanenta.edu.co>
2025-07-31 15:54:32 -0500

    se actualizaron arhivos

---
+++ ventana_1.py
@@ -0,0 +1,25 @@
+# se importa la libreria tkinter con todas sus funciones
+
+from tkinter import *
+
+#----------------------------------------------------
+# funciones de la app
+#----------------------------------------------------
+
+
+#-----------------------------------------
+# ventana principal
+#-----------------------------------------
+
+# se declara una variable llamada ventana_principal que adquiere las caracteristicas de un objeto TK()
+
+ventana_principal = Tk()
+
+# titulo de la ventana
+ventana_principal.title("juan fernando santana cala")
+
+# tamaño de la ventana
+ventana_principal.geometry("800x500")
+
+# metodo principal que despliega la ventana en la pantalla
+ventana_principal.mainloop()
