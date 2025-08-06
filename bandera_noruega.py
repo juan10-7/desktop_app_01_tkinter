742b8a4be318650eda6c1c5d45be567b085ce203
juan10-7 <juansantana1@colegioguanenta.edu.co>
2025-08-06 06:14:17 -0500

    archivos

--- bandera_noruega.py
+++ bandera_noruega.py
@@ -28,29 +28,45 @@ ventana_principal.resizable(0,0)
 # color de fondo de la pantalla
 ventana_principal.config(bg="black")
 
-#---------------
-# frame 1
-#---------------
+#-----------------------------
+# Frame 1
+# -----------------------------
 
 frame_1 = Frame(ventana_principal)
-frame_1.config(bg="yellow", width=780, height=240)
-frame_1.place(x=10,y=10)
+frame_1.config(bg="red", width=780, height=480)
+frame_1.place(x=10, y=10)
 
-#---------------
-# frame 2
-#---------------
+# -----------------------------
+# Frame 2
+# -----------------------------
 
 frame_2 = Frame(ventana_principal)
-frame_2.config(bg="blue", width=780, height=120)
-frame_2.place(x=10,y=250)
+frame_2.config(bg="white", width=100, height=480)
+frame_2.place(x=190, y=10)
 
-#---------------
-# frame 3
-#---------------
+# -----------------------------
+# Frame 3
+# -----------------------------
 
 frame_3 = Frame(ventana_principal)
-frame_3.config(bg="red", width=780, height=120)
-frame_3.place(x=10,y=370)
+frame_3.config(bg="white", width=780, height=100)
+frame_3.place(x=10,y=170)
+
+#-----------------------------
+# Frame 4
+# -----------------------------
+
+frame_4 = Frame(ventana_principal)
+frame_4.config(bg="darkblue", width=50, height=480)
+frame_4.place(x=215,y=10)
+
+# -----------------------------
+# Frame 5
+# -----------------------------
+
+frame_5 = Frame(ventana_principal)
+frame_5.config(bg="darkblue", width=780, height=50)
+frame_5.place(x=10,y=195)
 
 
 # metodo principal que despliega la ventana en la pantalla
--- pandera_francia.py
+++ pandera_francia.py
@@ -28,30 +28,29 @@ ventana_principal.resizable(0,0)
 # color de fondo de la pantalla
 ventana_principal.config(bg="black")
 
-#---------------
-# frame 1
-#---------------
+#-----------------------------
+# Frame 1
+# -----------------------------
 
 frame_1 = Frame(ventana_principal)
-frame_1.config(bg="yellow", width=780, height=240)
-frame_1.place(x=10,y=10)
+frame_1.config(bg="blue", width=260, height=480)
+frame_1.place(x=10, y=10)
 
-#---------------
-# frame 2
-#---------------
+# -----------------------------
+# Frame 2
+# -----------------------------
 
 frame_2 = Frame(ventana_principal)
-frame_2.config(bg="blue", width=780, height=120)
-frame_2.place(x=10,y=250)
+frame_2.config(bg="white", width=260, height=480)
+frame_2.place(x=270, y=10)
 
-#---------------
-# frame 3
-#---------------
+# -----------------------------
+# Frame 3
+# -----------------------------
 
 frame_3 = Frame(ventana_principal)
-frame_3.config(bg="red", width=780, height=120)
-frame_3.place(x=10,y=370)
-
+frame_3.config(bg="red", width=260, height=480)
+frame_3.place(x=530, y=10)
 
 # metodo principal que despliega la ventana en la pantalla
 ventana_principal.mainloop()
